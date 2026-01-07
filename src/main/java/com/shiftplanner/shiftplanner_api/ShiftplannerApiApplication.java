79a396cfee23ec2e295dfdfebdcf2a31362d9269
Verkäufer <mashayekhashkan89@gmail.com>
2026-01-07 16:26:21 +0100

    Projekt startet

---
+++ src/main/java/com/shiftplanner/shiftplanner_api/ShiftplannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.shiftplanner.shiftplanner_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftplannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftplannerApiApplication.class, args);
+	}
+
+}
